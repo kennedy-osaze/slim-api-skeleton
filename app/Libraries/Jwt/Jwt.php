cfffcd7ba87c42f62ef495e2474ba916ee73f6b8
Kennedy Osaze <me.osaze@gmail.com>
2019-07-26 15:48:23 +0100

    WIP: Complete changes in JWT without manual tests

--- app/Libraries/Jwt/Jwt.php
+++ app/Libraries/Jwt/Jwt.php
@@ -3,62 +3,104 @@
 namespace App\Libraries\Jwt;
 
 use Exception;
-use App\Models\User;
+use Carbon\Carbon;
 use InvalidArgumentException;
+use UnexpectedValueException;
 use Firebase\JWT\JWT as BaseJWT;
-use Illuminate\Database\Eloquent\Model;
-use App\Libraries\Jwt\Exceptions\JwtException;
-use App\Libraries\Jwt\Exceptions\InvalidConfigException;
-
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Firebase\JWT\{
+    ExpiredException, SignatureInvalidException, BeforeValidException
+};
+use App\Libraries\Jwt\Exceptions\{
+    JwtException,InvalidConfigException, TokenInvalidException, TokenExpiredException
+};
 
 class JWT extends BaseJWT
 {
+    /**
+     * Default configuration
+     *
+     * @var array
+     */
     protected $config = [
         'algo' => '',
         'secret' => '',
         'keys' => ['public' => '', 'private' => '', 'pass_phrase'],
         'leeway' => 0,
-        'token_lifetime' => 60,
+        'token_lifetime' => 60, // in minutes
         'authenticable' => User::class,
     ];
 
+    
+    protected $request;
+
     protected $payload = [];
 
-    public function __construct(array $config)
+    public function __construct(array $config, Request $request)
     {
         $this->config = array_merge($this->config, $config);
+        static::$leeway = $this->config['leeway'];
+
+        $this->request = $request;
 
         $this->validateConfig();
     }
 
-    public function generateToken(string $token_issuer = null, string $token_issued_to = null, array $claims = [])
+    public function getTokenForUser(JwtSubjectInterface $user, $accessing_from = null)
     {
+        return $this->generateToken($user->getJwtIdentifier(), $accessing_from, $user->getJwtCustomClaims());
+    }
+
+    public function generateToken(string $subject = null, string $for = null, array $custom_claims = [])
+    {
+        $payload = $this->makePayload(array_merge(['sub' => $subject, 'aud' => $for], $custom_claims));
+
         try {
-            $this->payload = [
-                'iss' => '',
-                'aud' => '',
-                'sub' => '',
-
-                'iat' => '',
-                'nbf' => '',
-                'exp' => ''
-            ];
-
-            return static::encode($this->payload, $this->getSigningKey(), $this->config['algo']);
+            return static::encode($payload, $this->getSigningKey(), $this->config['algo']);
         } catch (Exception $e) {
-            throw new JwtException('Unable to generate token', 100, $e);
+            throw new JwtException('Unable to generate token:' . $e->getMessage(), $e->getCode(), $e);
         }
+    }
+
+    protected function makePayload(array $claims)
+    {
+        $now = Carbon::now('UTC');
+
+        $payload = [
+            'iss' => rtrim(preg_replace('/\?.*/', '', (string) $this->request->getUri()), '/'),
+            'iat' => $now->getTimestamp(),
+            'nbf' => $now->getTimestamp(),
+            'exp' => $now->addMinutes((int) $this->config['token_lifetime'])->getTimestamp(),
+            'jti' => str_random(),
+        ];
+
+        $payload += $claims;
+
+        return array_filter($payload);
     }
 
     public function validateToken(string $token)
     {
-        // try {
+        try {
+            $this->payload = (array) static::decode(
+                $token, $this->getVerifyingKey(), (array) $this->config['algo']
+            );
+        } catch (ExpiredException $e) {
+            throw new TokenExpiredException('Token has expired', $e->getCode());
+        } catch (UnexpectedValueException | SignatureInvalidException | BeforeValidException $e) {
+            throw new TokenInvalidException('Unable to parse token: ' . $e->getMessage(), $e->getCode(), $e);
+        } catch (Exception $e) {
+            throw new JwtException('Unable to validate token: '. $e->getMessage(), $e->getCode(), $e);
+        }
+    }
 
-        // } catch () {
+    public function getUserByToken(string $token)
+    {
+        $this->validateToken($token);
 
-        // } catch () {
+        $authenticable = $this->config['authenticable'];
 
-        // }
+        return (new $authenticable)->getJwtTokenOwnerByIdentifier($this->payload['sub']);
     }
 
     protected function validateConfig()
@@ -79,8 +121,8 @@ class JWT extends BaseJWT
             throw new InvalidConfigException("The \"{$config['algo']}\" algorithm requires both a public key and a private key which could be a resource or a file path");
         }
 
-        if (!class_exists((string) $config['authenticable']) || !(new $config['authenticable'] instanceof Model)) {
-            throw new InvalidConfigException('The authenticable must be an instance of ' . Model::class);
+        if (!class_exists((string) $config['authenticable']) || !(new $config['authenticable'] instanceof JwtSubjectInterface)) {
+            throw new InvalidConfigException('The authenticable must be an instance of JwtSubjectInterface');
         }
     }
 
@@ -96,7 +138,7 @@ class JWT extends BaseJWT
     /**
      * Get the key used to sign the tokens.
      *
-     * @return resource|string
+     * @return string
      */
     protected function getSigningKey()
     {
@@ -104,20 +146,20 @@ class JWT extends BaseJWT
             return $this->config['secret'];
         }
 
-        $key_resource = openssl_get_privatekey(
-            $this->getKeyContent((string) $this->config['keys']['private']),
-            (string) $this->config['keys']['pass_phrase']
-        );
+        $key = (string) $this->config['keys']['private'];
+        $pass_phrase = (string) $this->config['keys']['pass_phrase'];
+
+        $key_resource = openssl_get_privatekey($this->getKeyContent($key), $pass_phrase);
 
         $this->validateKeyResource($key_resource);
 
-        return $key_resource;
+        return $key;
     }
 
     /**
      * Get the key used to verify the tokens.
      *
-     * @return resource|string
+     * @return string
      */
     protected function getVerifyingKey()
     {
@@ -125,11 +167,12 @@ class JWT extends BaseJWT
             return $this->config['secret'];
         }
 
-        $key_resource = openssl_get_publickey($this->getKeyContent((string) $this->config['keys']['public']));
+        $key = (string) $this->config['keys']['public'];
+        $key_resource = openssl_get_publickey($this->getKeyContent($key));
 
         $this->validateKeyResource($key_resource);
 
-        return $key_resource;
+        return $key;
     }
 
     /**
@@ -159,5 +202,15 @@ class JWT extends BaseJWT
         if (!isset($details['key']) || $details['type'] !== OPENSSL_KEYTYPE_RSA) {
             throw new InvalidArgumentException('Key has an invalid RSA signature');
         }
+    }
+
+    public function getRequest()
+    {
+        return $this->request;
+    }
+
+    public function getPayload()
+    {
+        return $this->payload;
     }
 }
